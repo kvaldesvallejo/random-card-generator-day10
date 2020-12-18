0c9cb5c2bae5107457b6f86c89f14995c1f06d76
kvaldesvallejo <kvaldesvallejo@gmail.com>
2020-12-18 01:44:54 +0000

    Finished

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
 import "./style.css";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  setTimeout(() => {
+    window.onload();
+  }, 2000);
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexNumber = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexNumber];
+};
+
+let generateRandomSuit = () => {
+  let suits = ["diamont", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suits.length);
+  return suits[indexSuit];
 };
